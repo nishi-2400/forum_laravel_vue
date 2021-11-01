e12aa0a5a3a52d503d210117fd3388b50a465de4
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-11-01 23:38:20 +0900

    ユニットテストセットアップ

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    public function store()
+    {
+        Contact::create(['name' => request('name')]);
+    }
+}
