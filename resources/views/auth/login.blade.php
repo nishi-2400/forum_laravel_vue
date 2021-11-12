b174ed112c465528c2de3f842e0b4367bdc9a80d
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-11-12 22:58:38 +0900

    Login page layout setup2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,57 +9,46 @@
 
         <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}" class="pt-8">
             @csrf
-
             <div class="relative">
                 <label for="email" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">E-mail</label>
-
-                <div class="">
-                    <input id="email" type="email" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-200 outline-none focus:bg-blue-700" name="email" value="{{ old('email') }}" required autofocus placeholder="your@email.com">
-
+                <div>
+                    <input id="email" type="email" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-200 outline-none focus:bg-blue-700" name="email" value="{{ old('email') }}"  autofocus placeholder="your@email.com">
                     @if ($errors->has('email'))
-                        <span class="" role="alert">
+                        <span class="text-red-600 text-sm pb-1" role="alert">
                             <strong>{{ $errors->first('email') }}</strong>
                         </span>
                     @endif
                 </div>
             </div>
-
             <div class="relative pt-3">
                 <label for="password" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">Password</label>
-
-                <div class="">
-                    <input id="password" type="password" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-200 outline-none focus:bg-blue-700" name="password" required placeholder="Password">
-
+                <div>
+                    <input id="password" type="password" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-200 outline-none focus:bg-blue-700" name="password"  placeholder="Password">
                     @if ($errors->has('password'))
-                        <span class="" role="alert">
+                        <span class="text-red-600 text-sm pb-1" role="alert">
                             <strong>{{ $errors->first('password') }}</strong>
                         </span>
                     @endif
                 </div>
             </div>
-
             <div class="pt-2">
-                <div class="">
-                    <div class="">
-                        <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                        <label class="text-white" for="remember">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                </div>
+                <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                <label class="text-white" for="remember">
+                    {{ __('Remember Me') }}
+                </label>
             </div>
-
-            <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                    <button type="submit" class="btn btn-primary">
-                        {{ __('Login') }}
-                    </button>
-
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        {{ __('Forgot Your Password?') }}
-                    </a>
-                </div>
+            <div class="pt-8">
+                <button type="submit" class="w-full bg-gray-300 uppercase rounded py-2 px-3 text-left text-blue-800 font-bold">
+                    {{ __('Login') }}
+                </button>
+            </div>
+            <div class="flex justify-between pt-8 text-white font-bold text-sm">
+                <a class="" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+                <a class="" href="{{ route('register') }}">
+                    Register
+                </a>
             </div>
         </form>
     </div>
