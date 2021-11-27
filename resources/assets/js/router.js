1971a19a855db4cec723a1b25ab43be68fdf3d9d
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-11-27 20:25:36 +0900

    contact show 実装

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -3,13 +3,15 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import ExampleComponentVue from "./components/ExampleComponent";
 import ContactCreate from "./views/ContactsCreate";
+import ContactsShow from "./views/ContactsShow";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
   routes: [
     { path: "/", component: ExampleComponentVue },
-    { path: "/contact/create", component: ContactCreate }
+    { path: "/contact/create", component: ContactCreate },
+    { path: "/contact/:id", component: ContactsShow }
   ],
   mode: "history" // #を削除
 });
