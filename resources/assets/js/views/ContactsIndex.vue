1a2a059fb1cdc817e22abe5600f4266362590523
nishi-2400 <a7w21087bump@yahoo.co.jp>
2021-12-03 22:12:29 +0900

    Show Contact List

---
+++ resources/assets/js/views/ContactsIndex.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <div v-if="contacts.length === 0">
+        <p>
+          No contacts yet.
+          <router-link to="/contact/create">Add contact</router-link>
+        </p>
+      </div>
+
+      <div v-for="contact in contacts">
+        <router-link
+          :to="'/contacts/' + contact.data.contact_id"
+          class="flex items-center border-b borer-gray-400 p-4 hover:bg-gray-100"
+        >
+          <UserCircle :name="contact.data.name"></UserCircle>
+          <div class="pl-4">
+            <p class="text-blue-400 font-bold">{{ contact.data.name }}</p>
+            <p class="text-gray-600">{{ contact.data.company }}</p>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import UserCircle from "../components/UserCircle";
+
+export default {
+  name: "ContactsIndex",
+  components: { UserCircle },
+  data: function() {
+    return {
+      contacts: {},
+      loading: true
+    };
+  },
+  mounted() {
+    axios
+      .get("/api/contacts")
+      .then(response => {
+        this.contacts = response.data.data;
+        this.loading = false;
+      })
+      .catch(error => {
+        console.log(error);
+        alert("Unable to fetch contact data");
+      });
+  }
+};
+</script>
